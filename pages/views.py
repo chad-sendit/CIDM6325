fcca354ea135b7e5d0f016ed305280373899afb0
unknown <chadsennett@gmail.com>
2022-09-11 17:38:25 -0500

    tests and pages for pages app

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,13 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 # Create your views here.
-def homePageView(request):
-    return HttpResponse('Hi, we are learning Django and Python')
+# def homePageView(request):
+#     return HttpResponse('Hi, we are learning Django and Python')
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
