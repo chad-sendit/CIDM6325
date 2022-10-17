12fe5c55ef956f9d1de576387f45484c5e0210c0
chad-sendit <chadsennett@gmail.com>
2022-10-17 11:35:06 -0500

    completed ch 5

--- Blog-App/blog/tests.py
+++ Blog-App/blog/tests.py
@@ -8,18 +8,18 @@ class BlogTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = get_user_model().objects.create_user(
-            username='testuser', email='test@test.com', password='secret' 
+            username="testuser", email="test@test.com", password="secret"
         )
 
         cls.post = Post.objects.create(
-            title = 'test post',
-            body = 'test body content',
-            author = cls.user,
+            title="test post",
+            body="test body content",
+            author=cls.user,
         )
 
     def test_post_model(self):
-        self.assertEqual(self.post.title, 'test post')
-        self.assertEqual(self.post.body, 'test body content')
-        self.assertEqual(self.post.author.username, 'testuser')
-        self.assertEqual(str(self.post), 'test post')
-        self.assertEqual(self.post.get_absolute_url(),'/post/1/')
+        self.assertEqual(self.post.title, "test post")
+        self.assertEqual(self.post.body, "test body content")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(str(self.post), "test post")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
